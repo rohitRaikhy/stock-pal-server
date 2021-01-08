6a50815a8bf52f854aa9b65479fee971db5542c8
Rohit Raikhy <raikhy101@gmail.com>
2021-01-07 20:27:53 -0500

    changed schema

--- daos/users.dao.server.js
+++ daos/users.dao.server.js
@@ -2,7 +2,7 @@ const usersModel = require("../models/Users/users.model.server")
 const findAllUsers = () => usersModel.find()
 const findUserById = (userID) => usersModel.findById(userID);
 const createUser = (userName, emailAddress, pass, gend, roleOf) => {
-   const user = {username: "test", email: "email", password: "pass", gender: "gender", role: "role"}
+   const user = {username: "test", email: "email", password: "pass", gender: "gender", role: "ADMIN"}
    return usersModel.create(user)
 }
 
